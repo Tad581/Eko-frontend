4a0f6c08bab998f65326a9dcfa51d34662268390
tad581 <nguyendat58211@gmail.com>
2023-06-28 21:37:02 +0700

    chore: thumbnail spellmiss fix

--- src/@core/components/home/thumbnail/index.tsx
+++ src/@core/components/home/thumbnail/index.tsx
@@ -42,7 +42,7 @@ export const Thumbnail = () => {
       }}
     >
       <Typography align="center" mb={5} sx={{ fontWeight: 600, fontSize: 40 }}>
-        いらっさいませ〜
+        いっらしゃいませ〜
       </Typography>
       <Stack
         direction="row"
